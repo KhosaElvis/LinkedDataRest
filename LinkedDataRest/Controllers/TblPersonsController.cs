7dc467391f70624bc91558b1e1f9d5eeed287d56
Elvis Khosa <elviskhosa@outlook.com>
2017-06-09 08:20:03 +0200

    Add project files.

---
+++ LinkedDataRest/Controllers/TblPersonsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LinkedDataRest.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LinkedDataRest.Controllers
+{
+    [Route("api/[controller]")]
+    public class TblPersonsController : Controller
+    {
+        private LinkDBContentContext db;
+
+        public TblPersonsController(LinkDBContentContext data)
+        {
+            this.db = data;
+        }
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<TblPerson> Get()
+        {
+            return db.TblPerson.ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ LinkedDataRest/Models/LinkDBContentContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace LinkedDataRest.Models
+{
+    public partial class LinkDBContentContext : DbContext
+    {
+        public virtual DbSet<TblPerson> TblPerson { get; set; }
+
+        public LinkDBContentContext(DbContextOptions<LinkDBContentContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblPerson>(entity =>
+            {
+                entity.ToTable("tblPERSON");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
